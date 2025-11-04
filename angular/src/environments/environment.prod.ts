11f825c64f60e6ce67fea11a199e072667dfde9d
nataliosorio <nataliaosorio973@gmail.com>
2025-11-04 00:39:06 -0500

    jenkis

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,11 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
+// export const environment = {
+//  production: false,
+//   development: true,
+//     apiURL: "https://localhost:7159/api",
+//     apiHub: "https://localhost:7159/parkingHub",
+
+
+
+//   //    production: true,
+//   // apiBase: '/api'
+// };
 export const environment = {
- production: false,
+  production: false,
   development: true,
-    apiURL: "https://localhost:7159/api",
-    apiHub: "https://localhost:7159/parkingHub",
 
-
-  //    production: true,
-  // apiBase: '/api'
+  apiURL: (window as any)['env']?.API_URL || 'http://localhost:5100/api',
+  apiHub: (window as any)['env']?.API_HUB || 'http://localhost:5100/parkingHub'
 };
+
